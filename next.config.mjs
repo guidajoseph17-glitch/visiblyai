b675b62249c389148cd76960065eac3a4d99b0b9
guidajoseph17-glitch <guida.joseph17@gmail.com>
2025-09-07 19:16:36 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: { ignoreDuringBuilds: true },   // <-- add this line
   experimental: {
     serverActions: { allowedOrigins: ['*'] }
   }
